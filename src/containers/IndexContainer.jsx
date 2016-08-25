0ec457a696fe6806fb56d093c4415df7459f82c7
Benjamin Kluck <ben@fug.lu>
2016-08-25 21:35:57 +0200

    1.0.5
    List style

--- src/containers/IndexContainer.jsx
+++ src/containers/IndexContainer.jsx
@@ -8,6 +8,12 @@ const style = {
 	height: '100vh',
 };
 
+const listStyle = {
+	border: '1px solid #ccc',
+	borderRadius: '10px',
+	padding: '20px',
+};
+
 const numberStyle = {
 	fontWeight: '600',
 	padding: '10px',
@@ -17,17 +23,19 @@ export default class IndexContainer extends React.Component {
 	render() {
 		return (
 			<div style={style}>
-				<div style={numberStyle}>
-					0211-123456
-				</div>
-				<div style={numberStyle}>
-					0211-123457
-				</div>
-				<div style={numberStyle}>
-					0211-123458
-				</div>
-				<div style={numberStyle}>
-					0211-123459
+				<div style={listStyle}>
+					<div style={numberStyle}>
+						0211-123456
+					</div>
+					<div style={numberStyle}>
+						0211-123457
+					</div>
+					<div style={numberStyle}>
+						0211-123458
+					</div>
+					<div style={numberStyle}>
+						0211-123459
+					</div>
 				</div>
 			</div>
 		);
