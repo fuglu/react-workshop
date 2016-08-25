a3e0937e07c3795a1d1cc6101f12bed7c320ac44
Benjamin Kluck <ben@fug.lu>
2016-08-25 20:37:18 +0200

    1.0.2
    Styles

--- src/containers/IndexContainer.jsx
+++ src/containers/IndexContainer.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+const style = {
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'center',
+	alignItems: 'center',
+	height: '100vh',
+};
+
 export default class IndexContainer extends React.Component {
 	render() {
 		return (
-			<div>
+			<div style={style}>
 				Hello world
 			</div>
 		);
