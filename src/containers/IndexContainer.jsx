1b21c2502d78b08b1339558ac9046a65fe38a243
Benjamin Kluck <ben@fug.lu>
2016-08-25 20:57:26 +0200

    1.0.4
    Number style

--- src/containers/IndexContainer.jsx
+++ src/containers/IndexContainer.jsx
@@ -8,20 +8,25 @@ const style = {
 	height: '100vh',
 };
 
+const numberStyle = {
+	fontWeight: '600',
+	padding: '10px',
+};
+
 export default class IndexContainer extends React.Component {
 	render() {
 		return (
 			<div style={style}>
-				<div>
+				<div style={numberStyle}>
 					0211-123456
 				</div>
-				<div>
+				<div style={numberStyle}>
 					0211-123457
 				</div>
-				<div>
+				<div style={numberStyle}>
 					0211-123458
 				</div>
-				<div>
+				<div style={numberStyle}>
 					0211-123459
 				</div>
 			</div>
